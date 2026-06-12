fc10b5b27c14ca4ba8fc1b705e485828d72f7e9e
sukunacutie <mjnavarro80@gmail.com>
2026-06-12 09:44:34 +0800

    "final commit"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import FeaturesSection from "@/components/Feature";
-import Footer from "@/components/Footer";
+
 import HeroSection from "@/components/Hero";
 import MobileAppSection from "@/components/Mobile";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
